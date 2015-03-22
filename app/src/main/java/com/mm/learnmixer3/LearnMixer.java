128349f626c9faf4e68e755cae10334ba92ccc84
Peter Szabó <szabo@expressprint.eu>
2015-03-22 22:16:04 +0100

    More stuff added

--- app/src/main/java/com/mm/learnmixer3/LearnMixer.java
+++ app/src/main/java/com/mm/learnmixer3/LearnMixer.java
@@ -1,5 +1,6 @@
 package com.mm.learnmixer3;
 
+import android.content.Context;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
@@ -10,9 +11,16 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+import android.widget.ArrayAdapter;
+import android.widget.ExpandableListView;
+
+import java.util.ArrayList;
 
 
 public class LearnMixer extends ActionBarActivity {
+    // Create ArrayList to hold parent Items and Child Items
+   // private ArrayList<String> parentItems = new ArrayList<String>();
+   // private ArrayList<Object> childItems = new ArrayList<Object>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,8 +31,78 @@ public class LearnMixer extends ActionBarActivity {
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
         }
+
+        // Create Expandable List and set it's properties
+       /* ExpandableListView expandableList = (ExpandableListView) findViewById(R.id.expandableListView1);// getExpandableListView();
+        expandableList.setDividerHeight(2);
+        expandableList.setGroupIndicator(null);
+        expandableList.setClickable(true);
+
+        // Set the Items of Parent
+        setGroupParents();
+        // Set The Child Data
+        setChildData();
+
+        // Create the Adapter
+        MyExpandableAdapter adapter = new MyExpandableAdapter(parentItems, childItems);
+
+        adapter.setInflater((LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE), this);
+
+        // Set the Adapter to expandableList
+        expandableList.setAdapter(adapter);
+        //expandableList.setOnChildClickListener(this);
+        */
     }
 
+   /* // method to add parent Items
+    public void setGroupParents()
+    {
+        parentItems.add("Fruits");
+        parentItems.add("Flowers");
+        parentItems.add("Animals");
+        parentItems.add("Birds");
+    }
+
+    // method to set child data of each parent
+    public void setChildData()
+    {
+
+        // Add Child Items for Fruits
+        ArrayList<String> child = new ArrayList<String>();
+        child.add("Apple");
+        child.add("Mango");
+        child.add("Banana");
+        child.add("Orange");
+
+        childItems.add(child);
+
+        // Add Child Items for Flowers
+        child = new ArrayList<String>();
+        child.add("Rose");
+        child.add("Lotus");
+        child.add("Jasmine");
+        child.add("Lily");
+
+        childItems.add(child);
+
+        // Add Child Items for Animals
+        child = new ArrayList<String>();
+        child.add("Lion");
+        child.add("Tiger");
+        child.add("Horse");
+        child.add("Elephant");
+
+        childItems.add(child);
+
+        // Add Child Items for Birds
+        child = new ArrayList<String>();
+        child.add("Parrot");
+        child.add("Sparrow");
+        child.add("Peacock");
+        child.add("Pigeon");
+
+        childItems.add(child);
+    }*/
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
